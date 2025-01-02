75b1157fd8f36310623c3245aa5ffb49a8aef5b1
willy <willymercier@outlook.fr>
2025-01-02 15:30:14 +0100

    Added tests for AreGridCoordinatesValid and GetElementFromGrid methods.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -23,12 +23,39 @@ public class Grid
         if(newElements.Length != this.gridElements.Length)
         {
             throw new ArgumentException("The quantity of newElements should match the quantity of gridElements set in the constructor. " +
-                "                       You input " + newElements.Length.ToString() + " newElements, while the gridElements is " +  gridElements.Length.ToString()+" .");
+                                        "You input " + newElements.Length.ToString() + " newElements, while the gridElements is " +  gridElements.Length.ToString()+" .");
         }
 
         for(int i = 0; i < newElements.Length; i++)
         {
             gridElements[i] = newElements[i];
         }
+    }
+
+    public void AddElement(GridElement element, int x, int y)
+    {
+        
+    }
+
+    public bool AreGridCoordinatesValid(int x, int y)
+    {
+        if (x < 0 || x > width - 1 || y < 0 || y > height - 1)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public GridElement GetElementFromGrid(int x, int y)
+    {
+        if(AreGridCoordinatesValid(x, y) == false)
+        {
+            throw new ArgumentException("The x and y coordinates you specified are beyond the scope of the grid.");
+        }
+
+        GridElement result = gridElements[width * y + x];
+
+        return result;
     }
 }
